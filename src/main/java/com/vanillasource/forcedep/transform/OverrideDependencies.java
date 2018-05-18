c1cec69ada2de0a1bfba5f79549126ea54a37513
Robert Brautigam <robert.brautigam.gyula@gmail.com>
2018-05-18 16:27:50 +0200

    Add implementation and extending translate to call

---
+++ src/main/java/com/vanillasource/forcedep/transform/OverrideDependencies.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright (C) 2018 VanillaSource
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.vanillasource.forcedep.transform;
+
+import com.vanillasource.forcedep.Dependencies;
+import java.util.concurrent.CompletableFuture;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+ * Simulates a <code>super</code> call for each overridden method. For interfaces too, where
+ * the call will go to the originating interface.
+ */
+public final class OverrideDependencies implements Dependencies {
+   private final Dependencies delegate;
+   private final Map<String, CompletableFuture<AnalyzedObject>> objects = new HashMap<>();
+
+   public OverrideDependencies(Dependencies delegate) {
+      this.delegate = delegate;
+   }
+
+   @Override
+   public Dependencies.Object object(String objectFqn, String... superObjectFqns) {
+      return new AnalyzedObject() {
+         private final Dependencies.Object object = delegate.object(objectFqn, superObjectFqns);
+         private final Set<String> methods = new HashSet<>();
+
+         @Override
+         public Dependencies.Method method(String methodName) {
+            methods.add(methodName);
+            Dependencies.Method method = new Dependencies.Method() {
+               private final Dependencies.Method method = object.method(methodName);
+
+               @Override
+               public void call(String objectsFqn, String methodName) {
+                  method.call(objectsFqn, methodName);
+               }
+
+               @Override
+               public void reference(String objectsFqn, String fieldName) {
+                  method.reference(objectsFqn, fieldName);
+               }
+
+               @Override
+               public void close() {
+                  method.close();
+               }
+            };
+            findOverridesFor(method, methodName);
+            return method;
+         }
+
+         @Override
+         public void potentiallyOverriddenBy(Dependencies.Method method, String methodName) {
+            if (methods.contains(methodName)) {
+               // Place call from overriding method to this overridden method
+               method.call(objectFqn, methodName);
+            } else {
+               // Try superclass
+               findOverridesFor(method, methodName);
+            }
+         }
+
+         @Override
+         public void close() {
+            object.close();
+            analyzedObjectFor(objectFqn).complete(this);
+         }
+
+         private void findOverridesFor(Dependencies.Method method, String methodName) {
+            for (String superObjectFqn: superObjectFqns) {
+               analyzedObjectFor(superObjectFqn)
+                  .thenAccept(obj -> obj.potentiallyOverriddenBy(method, methodName));
+            }
+         }
+
+         private CompletableFuture<AnalyzedObject> analyzedObjectFor(String objectFqn) {
+            return objects
+               .computeIfAbsent(objectFqn, k -> new CompletableFuture<>());
+         }
+      };
+   }
+
+   interface AnalyzedObject extends Dependencies.Object {
+      void potentiallyOverriddenBy(Dependencies.Method method, String methodName);
+   }
+}
+
---
+++ src/test/java/com/vanillasource/forcedep/transform/OverrideDependenciesTests.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (C) 2018 VanillaSource
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.vanillasource.forcedep.transform;
+
+import com.vanillasource.forcedep.Dependencies;
+import static org.mockito.Mockito.*;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+
+@Test
+public class OverrideDependenciesTests {
+   private Dependencies delegate;
+   private Dependencies.Object delegateObject;
+   private Dependencies.Method delegateMethod;
+   private OverrideDependencies dependencies;
+
+   public void testObjectsIsDelegated() {
+      dependencies.object("a.B", new String[] {});
+
+      verify(delegate).object("a.B", new String[] {});
+   }
+
+   public void testMethodsAreDelegated() {
+      dependencies
+         .object("a.B", new String[] {})
+         .method("c");
+
+      verify(delegateObject).method("c");
+   }
+
+   public void testMethodCallsAreDelegated() {
+      dependencies
+         .object("a.B", new String[] {})
+         .method("c")
+         .call("d.E", "f");
+
+      verify(delegateMethod).call("d.E", "f");
+   }
+
+   public void testSimpleOverrideWithOverridingFirstTranslatesToCall() {
+      Dependencies.Object o1 = dependencies.object("a.B", new String[] { "a.C" });
+      o1.method("b");
+      o1.close();
+      Dependencies.Object o2 = dependencies.object("a.C", new String[] {});
+      o2.method("b");
+      o2.close();
+
+      verify(delegateMethod).call("a.C", "b");
+   }
+
+   public void testSimpleOverrideWithOverriddenFirstTranslatesToCall() {
+      Dependencies.Object o2 = dependencies.object("a.C", new String[] {});
+      o2.method("b");
+      o2.close();
+      Dependencies.Object o1 = dependencies.object("a.B", new String[] { "a.C" });
+      o1.method("b");
+      o1.close();
+
+      verify(delegateMethod).call("a.C", "b");
+   }
+
+   public void testTransitiveOverrideWithOverridingFirstTranslatesToCall() {
+      Dependencies.Object o1 = dependencies.object("a.B", new String[] { "a.C" });
+      o1.method("b");
+      o1.close();
+      Dependencies.Object o2 = dependencies.object("a.C", new String[] { "a.D" });
+      o2.close();
+      Dependencies.Object o3 = dependencies.object("a.D", new String[] { });
+      o3.method("b");
+      o3.close();
+
+      verify(delegateMethod).call("a.D", "b");
+   }
+
+   @BeforeMethod
+   protected void setUp() {
+      delegate = mock(Dependencies.class);
+      delegateObject = mock(Dependencies.Object.class);
+      when(delegate.object(anyString(), anyVararg())).thenReturn(delegateObject);
+      delegateMethod = mock(Dependencies.Method.class);
+      when(delegateObject.method(anyString())).thenReturn(delegateMethod);
+      dependencies = new OverrideDependencies(delegate);
+   }
+}
+
